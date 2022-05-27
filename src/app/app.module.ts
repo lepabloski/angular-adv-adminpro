fe87f4e199a0f5dbd1bec5069662c86e94482366
Pablo Cabrol <pablo.cabrol@softvision.com>
2022-05-27 15:39:25 -0300

    pages and shared folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    NotFoundPageComponent,
+    DashboardComponent,
+    BreadcrumbComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
