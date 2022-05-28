9ac5d9777266e356b72e2e119b99ba7705d3bd67
Pablo Cabrol <pablo.cabrol@softvision.com>
2022-05-28 15:29:36 -0300

    register and login pages added, routing modified acording to the task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,22 +5,29 @@ import { RegisterComponent } from './auth/register/register.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Graphics1Component } from './pages/graphics1/graphics1.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { PagesComponent } from './pages/pages/pages.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graphic', component: Graphics1Component },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: '', redirectTo: 'dashboard' , pathMatch: 'full'},
+    ],
+  },
+
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'progress', component: ProgressComponent },
-  { path: 'graphic', component: Graphics1Component },
-  { path: '', redirectTo: 'dashboard' , pathMatch: 'full'},
-  { path: '**', component: NotFoundPageComponent},
+
+  { path: '**', component: NotFoundPageComponent },
 ];
 @NgModule({
   declarations: [],
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
