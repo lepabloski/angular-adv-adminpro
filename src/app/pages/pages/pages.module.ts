2139c20fb144d3b377242c785a4ab1802f858ca9
Pablo Cabrol <pablo.cabrol@softvision.com>
2022-05-31 08:49:24 -0300

    #progress bar logic added, i can increment o decrement

--- src/app/pages/pages/pages.module.ts
+++ src/app/pages/pages/pages.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { ProgressComponent } from '../progress/progress.component';
 import { Graphics1Component } from '../graphics1/graphics1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from 'src/app/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -21,6 +21,6 @@ import { AppRoutingModule } from 'src/app/app-routing.module';
     Graphics1Component,
     PagesComponent,
   ],
-  imports: [CommonModule, SharedModule, RouterModule],
+  imports: [FormsModule, CommonModule, SharedModule, RouterModule],
 })
 export class PagesModule {}
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styleUrls: ['./progress.component.css']
+  styleUrls: ['./progress.component.css'],
 })
-export class ProgressComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ProgressComponent {
+  progress: number = 50;
+  // we create a getter to access the progress
+  get getProgress() {
+    return `${this.progress}%`;
   }
 
+  changeProgress = (value: number) => {
+    console.log(this.progress);
+    // range plus validation
+    if (this.progress >= 100 && value >= 0) {
+      return (this.progress = 100);
+    }
+    // reange minus validation
+    if (this.progress <= 0 && value < 0) {
+      return (this.progress = 0);
+    }
+    // change the value o progress depending on value
+
+    return (this.progress = this.progress + value);
+  };
 }
