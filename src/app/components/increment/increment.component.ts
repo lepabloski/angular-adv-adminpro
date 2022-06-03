6009191df00c6bb772c332dfdb9175c62cfb71bc
Pablo Cabrol <pablo.cabrol@softvision.com>
2022-06-03 11:23:02 -0300

    progress bar and ingremet widget related with eventemitters

--- src/app/components/increment/increment.component.ts
+++ src/app/components/increment/increment.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-increment',
@@ -6,25 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./increment.component.css']
 })
 export class IncrementComponent {
-  progress: number = 50;
-  // we create a getter to access the progress
-  get getProgress() {
-    return `${this.progress}%`;
-  }
+  // @Input('valor') progress: number = 50 para re definir el valor del input
+  @Input('valor') progress: number = 50;
+// la salida en es EventEmitter en cualva a emitir un evento de tipo numero, 
+// lo signo valor para evitar el null o el undefined
+  @Output() valorSalida: EventEmitter<number> = new EventEmitter
 
   changeProgress = (value: number) => {
     console.log(this.progress);
     // range plus validation
     if (this.progress >= 100 && value >= 0) {
-      return (this.progress = 100);
+      this.valorSalida.emit(100)
+      return (this.progress = 100)
     }
     // reange minus validation
     if (this.progress <= 0 && value < 0) {
-      return (this.progress = 0);
+      this.valorSalida.emit(0)
+      return (this.progress = 0)
     }
     // change the value o progress depending on value
-
-    return (this.progress = this.progress + value);
+    
+    this.valorSalida.emit(this.progress)
+   return this.progress = this.progress + value
   };
 
 }
